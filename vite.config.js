62e62b8ed2d1488b649318954f6c78827d233e70
Nhat Sang Le <nhatsang2905@gmail.com>
2024-05-28 08:16:23 +0700

    chore: Update page title and set default open path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: '/admin/dashboard', // Đặt đường dẫn mặc định khi mở trên server
+  },
+});
