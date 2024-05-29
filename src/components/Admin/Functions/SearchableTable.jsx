03e914711d49b558fc9fa97cf69559de9a7bc550
Nhat Sang Le <nhatsang2905@gmail.com>
2024-05-30 05:17:44 +0700

    Sang/Add Search Function For all table

---
+++ src/components/Admin/Functions/SearchableTable.jsx
@@ -0,0 +1,44 @@
+import { Input, Table } from 'antd';
+import React from 'react';
+
+class SearchableTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: '',
+            filteredData: props.data,
+        };
+    }
+
+    handleSearch = (value) => {
+        const searchQuery = value.toLowerCase();
+        const filtered = this.props.data.filter((record) => {
+            return Object.values(record).some((field) =>
+                field.toString().toLowerCase().includes(searchQuery)
+            );
+        });
+        this.setState({ filteredData: filtered });
+    };
+
+    render() {
+        return (
+            <>
+                <Input.Search
+                    placeholder="Tìm kiếm"
+                    allowClear
+                    enterButton="Search"
+                    size="large"
+                    onSearch={this.handleSearch}
+                    style={{ marginBottom: 16 }}
+                />
+                <Table
+                    columns={this.props.columns}
+                    dataSource={this.state.filteredData}
+                    {...this.props.tableProps} // Các thuộc tính khác của Table
+                />
+            </>
+        );
+    }
+}
+
+export default SearchableTable;
